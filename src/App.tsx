1a3b7a50a3cc9550d8d6806c31f05ebe2e873600
Sense <105939929+0xSense@users.noreply.github.com>
2023-12-20 14:59:09 -0500

    build: solana create install

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Gaming Market
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <br></br>
+      <div>
+        Bonk Bouncer
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="button-mint" >
+        Mint
+      </button>
     </>
   )
 }
