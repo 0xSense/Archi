ca4b28a30f05e7ff1d76a704e81c484d5de04aff
0xSense <105939929+0xSense@users.noreply.github.com>
2023-12-27 15:38:43 -0500

    feat: fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Gaming Market
+      <div className='site-name'>
+        Archi
       </div>
       <br></br>
       <div>
-        Bonk Bouncer
+        Games
       </div>
-      <button className="button-mint" >
-        Mint
-      </button>
     </>
   )
 }
